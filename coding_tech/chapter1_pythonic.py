f3bccdfabc7f1ddd06bef31b6dfaf1d8d8975a0d
YuJinsoo <realaqua23@gmail.com>
2023-10-24 17:49:03 +0900

    파이썬 코딩의 기술 1-1

---
+++ coding_tech/chapter1_pythonic.py
@@ -0,0 +1,6 @@
+# 1
+import sys
+print(sys.version_info) 
+# sys.version_info(major=3, minor=9, micro=13, releaselevel='final', serial=0)
+print(sys.version)
+# 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)]
